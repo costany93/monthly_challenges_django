5ac1506d58feeec85f1690a0e29cf2ef8bb65627
costany93 <kandzaprince@gmail.com>
2025-01-03 19:27:09 +0000

    Premier pas avec Django

---
+++ challenges/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+
+# on import du dossiers courant notre fichier views
+from . import views
+
+# ici on defini nos urls
+urlpatterns = [
+    # ici on definit notre url janvier qui est attachee a notre vu index du dossier challenge, c'est un url static
+    # path("janvier", views.index),
+    # path("fevrier", views.fevrier)
+
+
+    # ici on aura que les entier comme parametres acceptables NB ici il s'agit d'uune route ou d'un url non nommee no-named url
+    #  path("<int:month>", views.monthly_challenges_by_month),
+    # ici travaillant avec un named url
+    path("<int:month>", views.monthly_challenges_by_month),
+    # par defaut cette config d'url sans str pouvait revoir tout type de donnee qui pouvait etre passee en formattant avec le str cette url ne pourra plus que recevoir les string en parametre. NB ici il s'agit d'uune route ou d'un url non nommee no-named url
+    path("<str:month>", views.monthly_challenge, name="month-challenges"),
+    path("", views.index)
+
+
+
+]
+
+# nous devons rendre cette url disponible dans toute notre application on l'ajoutant au fichier urls du dossier principal de notre application(monthly_challenges)
---
+++ challenges/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+# from django.template.loader import render_to_string
+
+monthly_challenges = {
+    "janvier": "Faire du sport pendant tout le mois",
+    "fevrier": "Faire beaucoup de cardion afin de perdre du poids au moins 2 kilo",
+    "mars": "Preparer ses objectifs pour l'ete",
+    "avril": "Boire beaucoup d'eau",
+    "mai": "Visiter un musee",
+    "juin": "Apprendre angular",
+    "juillet": "Lire 3 livres",
+    "aout": "Manger beaucoup de poisson",
+    "septembre": "Beaucoup nager",
+    "octobre": "Visiter de regions du pays",
+    "novembre": "Apprendre une nouvelle competence",
+    "decembre": None,
+
+}
+
+# Create your views here.
+
+# definissons notre fonction index
+
+
+# def index(request):
+#     return HttpResponse("It's working")
+
+# # ici on definit un fonction qui retourne un url static
+# def fevrier(request):
+#     return HttpResponse("Fevrier")
+
+def index(request):
+    list_item = ""
+    months = list(monthly_challenges.keys())
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
+
+
+def monthly_challenge(request, month):
+    # ici on teste si le moiss passer en parametre d'url est  dans notre liste de mois
+    # try:
+    challenge_text = monthly_challenges[month]
+    # ici on definit la reponse dans notre templates challenges/templates/challenge.html
+    return render(request, 'challenges/challenge.html', {
+        "text": challenge_text,
+        "month": month
+    })
+
+    # response_data = render_to_string("challenges/challenge.html")
+    return HttpResponse(response_data)
+    # except:
+    return HttpResponseNotFound("Ce mois n'est pas disponible")
+
+
+# ici on definit un fonction qui retourne des url dynamique, le parametre month est aussi defini comme le nom de notre url dans le fichier urls
+# C'EST LA FONCTION DU HAUT MAIS SANS AVOIRR UTILISER UNE LISTE PYTHON
+# def monthly_challenges(request, month):
+#     # ici om va passer dynamiquement un nos objectfis en fonction des mois
+#     challenge_text = None
+#     if month == "janvier":
+#         challenge_text = "Faire du sport pendant tout le mois"
+#     elif month == "fevrier":
+#         challenge_text = "Faire beaucoup de cardion afin de perdre du poids au moins 2 kilo"
+#     elif month == "mars":
+#         challenge_text = "Preparer ses objectifs pour l'ete"
+#     else:
+#         challenge_text = "Les mois n'est pas encore prit en charge"
+#     return HttpResponse(challenge_text)
+
+# ici nous definissons notre fonction qui recoit a la place du mois en lettre, le moiss en chiffre en formattant son url a ne recevoir que les chiffres
+
+
+def monthly_challenges_by_month(request, month):
+    # ici on recupere les cles de notre liste(janvier, fevrier, etc...)
+    months = list(monthly_challenges.keys())
+    # ici on teste si nous pasons un nombre qui n'est pas valide en fonction du mois
+    if month > len(months):
+
+        return HttpResponseNotFound("Le mois entrer n'existe pas")
+        # ici je vais faire matcher les cles rrecuperes avec l'arguments passer dans la requette
+    recup_month = months[month - 1]
+
+    # ici on utilise un named url afin de dynamiser le site
+    url_month = reverse("month-challenges", args=[recup_month])
+    return HttpResponseRedirect(url_month)
+    # ici je redirige l'utilisateur en fonction du mois en chiffre passer comme argument a la page qu'il faut
